de5cb16b52055b60dc71c3088773fea2dcde86f0
Unknown <guileme13@gmail.com>
2019-02-15 13:33:15 -0200

    setup projeto

---
+++ backend/src/main/java/com/gcl/myt/model/User.java
@@ -0,0 +1,39 @@
+package com.gcl.myt.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", uniqueConstraints = {
+	@UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Email
+	@Column(nullable = false)
+	private String email;
+	
+	@JsonIgnore
+	private String password;
+	
+}
